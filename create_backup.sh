dd41c93e64a986e4661f65d7bad148dd5fece986
IncSow <helynt@dsifrance.fr>
2022-11-05 02:42:57 +0100

    :sparkles: Now automatically creates my backups!

---
+++ create_backup.sh
@@ -0,0 +1,6 @@
+cp ~/.config/i3/config config
+cp /usr/bin/set_i3_theme.sh set_i3_theme.sh
+cp ~/.local/share/cool-retro-term/cool-retro-term/QML/OfflineStorage/Databases/27e743fe85b8912a46804fed99e8a9ab.sqlite 27e743fe85b8912a46804fed99e8a9ab.sqlite
+gadd
+git commit "Automatic commit from $USER"
+git push
