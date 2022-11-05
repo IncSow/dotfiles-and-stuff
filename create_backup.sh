e4eaf7ac5dc0cc04d5f273b2344b67bea5532963
IncSow <helynt@dsifrance.fr>
2022-11-05 02:45:08 +0100

    Automatic commit from lilo

--- create_backup.sh
+++ create_backup.sh
@@ -1,6 +1,7 @@
+cp ~/.bashrc .bashrc
 cp ~/.config/i3/config config
 cp /usr/bin/set_i3_theme.sh set_i3_theme.sh
 cp ~/.local/share/cool-retro-term/cool-retro-term/QML/OfflineStorage/Databases/27e743fe85b8912a46804fed99e8a9ab.sqlite 27e743fe85b8912a46804fed99e8a9ab.sqlite
-gadd
-git commit "Automatic commit from $USER"
+git add .
+git commit -m  "Automatic commit from $USER"
 git push
