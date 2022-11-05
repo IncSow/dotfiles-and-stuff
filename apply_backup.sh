5bf135dabb5fe72f3e6be90e29b9a576e07ede3f
IncSow <helynt@dsifrance.fr>
2022-11-05 02:48:10 +0100

    Automatic commit from lilo

---
+++ apply_backup.sh
@@ -0,0 +1,5 @@
+git pull origin master
+cp .bashrc ~/
+cp config ~/.config/i3
+cp set_i3_theme.sh /usr/bin/
+cp 27e743fe85b8912a46804fed99e8a9ab.sqlite ~/.local/share/cool-retro-term/cool-retro-term/QML/OfflineStorage/Databases/ 
