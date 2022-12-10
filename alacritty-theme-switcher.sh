6eb03ad353f09c1a80c0296cb836b43589e10659
IncSow <helynt@dsifrance.fr>
2022-12-10 18:57:17 +0100

    Backing up stuff

--- alacritty-theme-switcher.sh
+++ alacritty-theme-switcher.sh
@@ -4,4 +4,5 @@ rng=`(date +%s)`
 rng=`expr $rng % $theme_length`
 choice=${theme_list[$rng]}
 notify-send "Theme has been changed to $choice"
+echo "Your theme has been switched to $choice"
 sed -i "/colors:\ /c\colors: *$choice" ~/.config/alacritty/alacritty.yml
